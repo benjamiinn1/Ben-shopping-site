beecf0c7562e35943b8a9afd866a40bb5270b289
benjamiinn1 <33909077+benjamiinn1@users.noreply.github.com>
2020-12-07 20:45:54 -0600

    added router to frontend

--- shopping-frontend/src/components/Header.js
+++ shopping-frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,16 +7,20 @@ const Header = () => {
     <header>
       <Navbar bg='primary' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Ben's Store</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Ben's Store</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart' className='fas fa-shopping-cart'>
-                Cart
-              </Nav.Link>
-              <Nav.Link href='/login' className='fas fa-user'>
-                Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link className='fas fa-shopping-cart'>Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link href='/login' className='fas fa-user'>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
