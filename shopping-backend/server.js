119d49d2955a99e0db8124db354b3bd225cf5622
benjamiinn1 <33909077+benjamiinn1@users.noreply.github.com>
2020-12-10 21:05:52 -0600

    created mongoose schemas for products, users, and orders

--- shopping-backend/server.js
+++ shopping-backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,5 +27,7 @@ app.get('/api/products/:id', (req, res) => {
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold
+  )
 )
