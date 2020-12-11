e19b3e82d6b6bf7b971e0054137ca585f81f83fd
benjamiinn1 <33909077+benjamiinn1@users.noreply.github.com>
2020-12-10 20:06:34 -0600

    added env variables

--- shopping-backend/server.js
+++ shopping-backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
+
 const app = express()
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('API is running')
+  res.send('API is running...')
 })
 
 app.get('/api/products', (req, res) => {
@@ -17,4 +21,7 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(PORT, console.log(`Server running on PORT ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+)
